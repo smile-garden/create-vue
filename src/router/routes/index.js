360feaa644e8eebbb84a972ec919acbc874d0656
fenglingcong <1539027251@qq.com>
2020-04-16 20:28:37 +0800

    'iview'

--- src/router/routes/index.js
+++ src/router/routes/index.js
@@ -5,6 +5,11 @@ export const constantRoutes = [
     component: () => import('@/views/home/Index'),
   },
   {
+    path: '/practice',
+    name: 'practice',
+    component: () => import('@/views/practice/Index'),
+  },
+  {
     path: '/exception/404',
     name: 'exception404',
     component: () => import('@/views/exception/404'),
---
+++ src/views/practice/Index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="list">
+    <Card title="Button" class="item">
+      <Button>Default</Button>
+      <Button type="primary">Primary</Button>
+      <Button type="dashed">Dashed</Button>
+      <Button type="text">Text</Button>
+      <br><br>
+      <Button type="info">Info</Button>
+      <Button type="success">Success</Button>
+      <Button type="warning">Warning</Button>
+      <Button type="error">Error</Button>
+    </Card>
+    <Card title="Icon" class="item" style="font-size: 48px;">
+      <Icon type="ios-alarm" />
+      <Icon type="ios-american-football" />
+      <Icon type="ios-aperture" />
+    </Card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'practice',
+  data() {
+    return {
+    };
+  },
+  created() {},
+  methods: {},
+};
+</script>
+
+<style lang='less' scoped>
+.item {
+  margin-bottom: 10px;
+}
+
+</style>
